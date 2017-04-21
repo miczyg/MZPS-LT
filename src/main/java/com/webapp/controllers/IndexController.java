491e63e16b58de9c5446169825c644b9dc641098
wegrzyns <krzysiu@smsnet.pl>
2017-04-21 21:39:24 +0200

    Added spring with basic functionality.

---
+++ src/main/java/com/webapp/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.webapp.controllers;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String greeting(Device device) {
+
+        return "index";
+    }
+}
---
+++ src/main/resources/templates/mobile/index.ftl
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Getting Started: Serving Web Content</title>
+</head>
+<body>
+<h1>You are into mobile version</h1>
+<p>Get your greeting <a href="/greeting">here</a></p>
+</body>
+</html>
