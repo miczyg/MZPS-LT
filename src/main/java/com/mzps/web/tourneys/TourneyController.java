885e07f0ab7e69f4ccc29c83eaa40f96e308ea06
miczyg <miczyg13@gmail.com>
2017-05-24 23:50:21 +0200

    [WIP] tourneys controllers and mapping

--- src/main/java/com/mzps/web/tourneys/TourneyController.java
+++ src/main/java/com/mzps/web/tourneys/TourneyController.java
@@ -1,8 +1,12 @@
 package com.mzps.web.tourneys;
 
+import com.mzps.model.League;
 import com.mzps.model.MatchResult;
 import com.mzps.model.Team;
+import com.mzps.model.Tourney;
 import com.mzps.util.CustomErrorType;
+import com.mzps.web.admin.LeagueService;
+import com.mzps.web.admin.TourneyService;
 import com.mzps.web.teams.TeamController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,15 +26,33 @@ public class TourneyController {
     public static final Logger logger = LoggerFactory.getLogger(TourneyController.class);
 
     @Autowired
+    TourneyService tourneyService;
+
+    @Autowired
+    LeagueService leagueService;
+
+    @Autowired
     MatchResultService matchResultService;
 
     // -------------------Retrieve All MatchResults---------------------------------------------
     @GetMapping(value = "/")
-    public ResponseEntity<List<MatchResult>> listAllMatchResults() {
-        List<MatchResult> matchResults = matchResultService.findAllMatchResults();
-        if (matchResults.isEmpty()) {
+    public ResponseEntity<List<Tourney>> getTourneys() {
+        List<Tourney> tourneys = tourneyService.findAllTourneys();
+        if (tourneys.isEmpty()) {
             return new ResponseEntity(HttpStatus.NO_CONTENT);
         }
-        return new ResponseEntity<>(matchResults, HttpStatus.OK);
+        return new ResponseEntity<>(tourneys, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{tourneyId}/{leagueId}")
+    public ResponseEntity<League> getLeague(@PathVariable Long leagueId){
+        League league = leagueService.findById(leagueId);
+        return new ResponseEntity<>(league, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/tourneyId}/{leagueId}/{matchId}")
+    public ResponseEntity<MatchResult> getMatchResult(@PathVariable Long matchId){
+        MatchResult result = matchResultService.findById(matchId);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
